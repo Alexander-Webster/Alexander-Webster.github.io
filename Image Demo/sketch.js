5819b121252ac1eee8622e5b8080b576c05c3736
Alexander-Webster <60943818+Alexander-Webster@users.noreply.github.com>
2020-02-12 15:03:10 -0600

    Added zoom

--- Image Demo/sketch.js
+++ Image Demo/sketch.js
@@ -6,6 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 let ratFood;
+let scalar = 0.10;
 
 function preload(){
   ratFood = loadImage("assets/Rat Food.jpg");
@@ -20,5 +21,15 @@ function draw() {
   background(220);
   fill("red");
   // rect(mouseX, mouseY, 75, 75);
-  image(ratFood, mouseX, mouseY);
+  image(ratFood, mouseX, mouseY, scalar * ratFood.width, scalar * ratFood.height);
+}
+function mouseWheel() {
+  print(event.delta);
+  if(event.delta >= 0){
+    scalar *= 1.1;
+  }
+
+  if(event.delta < 0){
+    scalar *= 0.9;
+  }
 }
