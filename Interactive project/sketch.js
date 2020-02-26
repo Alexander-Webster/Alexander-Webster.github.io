c026a050db11ed3bbbb525205b4ffefe3e07c665
Alexander-Webster <60943818+Alexander-Webster@users.noreply.github.com>
2020-02-26 14:43:48 -0600

    Added a WASD code

--- Interactive project/sketch.js
+++ Interactive project/sketch.js
@@ -8,6 +8,16 @@
 
 let entity;
 
+let x;
+let y;
+let dx = 5;
+let dy = 5;
+let movingUp = false;
+let movingDown = false;
+let movingLeft = false;
+let movingRight = false;
+
+
 function preload(){
   entity = loadImage("assets/Millenium Falcon.jpg");
 }
@@ -24,7 +34,58 @@ function draw() {
 }
 
 function createShip(){
-  image(entity, mouseX, mouseY);
+  image(entity, x, y);
+
 }
 
+function keyPressed() {
+  if (key === "w") {
+    movingUp = true;
+  }
+  if (key === "a") {
+    movingLeft = true;
+  }
+  if (key === "s") {
+    movingDown = true;
+  }
+  if (key === "d") {
+    movingRight = true;
+  }
+}
+
+function keyReleased() {
+  if (key === "w") {
+    movingUp = false;
+  }
+  if (key === "a") {
+    movingLeft = false;
+  }
+  if (key === "s") {
+    movingDown = false;
+  }
+  if (key === "d") {
+    movingRight = false;
+  }
+}
+
+
+function moveBox() {
+  if (movingUp) {
+    y -= dy;
+  }
+  if (movingLeft) {
+    x -= dx;
+  }
+  if (movingDown) {
+    y += dy;
+  }
+  if (movingRight) {
+    x += dx;
+  }
+}
+
+function displayBox() {
+  fill("black");
+  rect(x, y, 50, 50);
+}
 
