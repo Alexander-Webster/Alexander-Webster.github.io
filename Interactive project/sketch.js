dd463014ffdb17c35e008d0f55450c13ed6933a2
Alexander-Webster <60943818+Alexander-Webster@users.noreply.github.com>
2020-03-02 15:19:15 -0600

    Image shows up- change controls next

--- Interactive project/sketch.js
+++ Interactive project/sketch.js
@@ -16,7 +16,7 @@ let movingUp = false;
 let movingDown = false;
 let movingLeft = false;
 let movingRight = false;
-let scalar = 0.7;
+let scalar = 0.2;
 
 function preload(){
   entity = loadImage("assets/Millenium Falcon.jpg");
@@ -30,9 +30,10 @@ function setup() {
 }
 
 function draw() {
+  background(220);
   moveEntity();
   createShip();
-  background(220);
+  
 }
 
 
@@ -55,6 +56,7 @@ function createShip(){
 function keyPressed() {
   if (key === "w") {
     movingUp = true;
+    
   }
   if (key === "a") {
     movingLeft = true;
