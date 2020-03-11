3e153094fc7597e71f0b46ac1859a64e11924737
Alexander-Webster <60943818+Alexander-Webster@users.noreply.github.com>
2020-03-11 14:56:39 -0600

    Switched file names to make sense

--- Template/sketch.js
+++ Template/sketch.js
@@ -5,31 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let white = true;
+
 function setup() {
-  if(windowWidth > windowHeight){
-    cre
-  }
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
-  for (let i = 0; i < width; i += 100){
-    for(let e = 0; e < height; e += 100){
-      if(white){
-        fill("white");
-      }
-      else {
-        fill("black");
-      }
-      rect(i, e, 80, 80);
-      white = !white;
-    }
-    white = !white
-  }
-}
-
-
-function windowResized(){
-  setup();
 }
